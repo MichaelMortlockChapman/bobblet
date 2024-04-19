6b8ed31b793038df17e0728ba69e177b2b70961c
MichaelMortlockChapman <mmortlockchapman@gmail.com>
2024-04-19 18:27:55 +1000

    cleaned up stacks into component

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -9,29 +9,32 @@ import * as THREE from 'three'
 export function Scene(props) {
   const { nodes, materials } = useGLTF('/bobblet.glb')
   
-  // const playerStacks = [[4, 4, 4], [4, 4, 4]]
-
-  // const createStackPiece = (positionXZZ, player, piece) => {
-  //   const playerString = player == 0 ? 'White' : 'Black'
-  //   return ( <>
-  //     {piece > 0 && <mesh
-  //       castShadow
-  //       receiveShadow
-  //       geometry={ghostRef.current.geometry = nodes[`${playerString}${piece}`].geometry}
-  //       material={materials.WhitePiece}
-  //       position={[0, 0.598, -3.146]}
-  //     />}
-  //   </>)
-  // }
-
   const pieceHeights = [1.181, 1.326, 1.399, 1.417]
+  const boardWidth = 1.55; const boardDepth = 1.55;
+  const boardWidthDif = 1.035; const boardDepthDif = 1.035
+  const boardHeight = 0.85
+
+  const playerStacks = [[4, 4, 4], [4, 4, 4]]
+
+  const Piece = ({positionXZ, player, piece}) => {
+    const playerString = player == 0 ? 'White' : 'Black'
+    return ( <>
+      {piece > 0 && <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes[`${playerString}${piece}`].geometry}
+        material={player == 0 ? materials.WhitePiece : materials.BlackPiece}
+        position={[positionXZ[0], pieceHeights[piece - 1] - 0.8, positionXZ[1]]}
+      />}
+    </>)
+  }
+
   const ghostRef = useRef()
   const ghostBlackMat = materials.BlackPiece.clone()
-  ghostBlackMat.transparent = true
-  ghostBlackMat.opacity = 0.3
+  ghostBlackMat.transparent = true; ghostBlackMat.opacity = 0.3;
   const ghostWhiteMat = materials.WhitePiece.clone()
-  ghostWhiteMat.transparent = true
-  ghostWhiteMat.opacity = 0.3
+  ghostWhiteMat.transparent = true; ghostWhiteMat.opacity = 0.3;
+
   // used to show a hover ghost on placement
   const updateGhostPlacement = (positionXZ, player, piece) => {
     if (ghostRef.current == null) {
@@ -52,41 +55,38 @@ export function Scene(props) {
     ghostRef.current.visible = false
   }
 
-    // make buttons (boxes) for each board cell
-    const boardWidth = 1.55; const boardDepth = 1.55;
-    const boardWidthDif = 1.035; const boardDepthDif = 1.035
-    const boardHeight = 0.85
-    const createButtons = () => {
-      const buttons = []
-      for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-          buttons.push(
-            <mesh 
-              key={`${i} ${j}`} 
-              position={new THREE.Vector3(boardWidth - i * boardWidthDif, boardHeight, boardDepth - j * boardDepthDif)}
-              onClick={(e) => {
-                e.stopPropagation()
-                console.log(`${i} ${j}`);
-              }}
-              onPointerEnter={(e) => updateGhostPlacement([boardWidth - i * boardWidthDif, boardDepth - j * boardDepthDif], 0, 4)}
-              onPointerLeave={(e) => hideGhost()}
-              visible={false}
-            >
-              <boxGeometry args={[0.75,0.1,0.75]}/>
-            </mesh>
-          )
-        }
+  // make buttons (boxes) for each board cell
+  const createButtons = () => {
+    const buttons = []
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        buttons.push(
+          <mesh 
+            key={`${i} ${j}`} 
+            position={new THREE.Vector3(boardWidth - i * boardWidthDif, boardHeight, boardDepth - j * boardDepthDif)}
+            onClick={(e) => {
+              e.stopPropagation()
+              console.log(`${i} ${j}`);
+            }}
+            onPointerEnter={(e) => updateGhostPlacement([boardWidth - i * boardWidthDif, boardDepth - j * boardDepthDif], 0, 4)}
+            onPointerLeave={(e) => hideGhost()}
+            visible={false}
+          >
+            <boxGeometry args={[0.75,0.1,0.75]}/>
+          </mesh>
+        )
       }
-      return buttons
     }
+    return buttons
+  }
     
-    return (
-      <group {...props} dispose={null}>
+  return (
+    <group {...props} dispose={null}>
       <mesh
         className={"GhostPiece"}
         castShadow
         receiveShadow
-        visible={true}
+        visible={false}
         ref={ghostRef}
         geometry={nodes.Black1.geometry}
         material={ghostWhiteMat}
@@ -103,49 +103,13 @@ export function Scene(props) {
         {createButtons()}
       </group>
       {/* <mesh castShadow receiveShadow geometry={nodes.Table.geometry} material={materials.Table} /> */}
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.BlackStackRef2.geometry}
-        material={materials.BlackPiece}
-        position={[0, 0.598, 3.5]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.BlackStackRef3.geometry}
-        material={materials.BlackPiece}
-        position={[1.107, 0.598, 3.5]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.BlackStackRef1.geometry}
-        material={materials.BlackPiece}
-        position={[-1.189, 0.598, 3.5]}
-      />
-      {}
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.WhiteStackRef1.geometry}
-        material={materials.WhitePiece}
-        position={[-1.189, 0.598, -3.146]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.WhiteStackRef3.geometry}
-        material={materials.WhitePiece}
-        position={[1.107, 0.598, -3.146]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.WhiteStackRef2.geometry}
-        material={materials.WhitePiece}
-        position={[0, 0.598, -3.146]}
-      />
+      <Piece positionXZ={[-1.2, 3.1]} player={0} piece={playerStacks[0][0]}/>
+      <Piece positionXZ={[0, 3.1]} player={0} piece={playerStacks[0][1]}/>
+      <Piece positionXZ={[1.2, 3.1]} player={0} piece={playerStacks[0][2]}/>
+
+      <Piece positionXZ={[-1.2, -3.1]} player={1} piece={playerStacks[1][0]}/>
+      <Piece positionXZ={[0, -3.1]} player={1} piece={playerStacks[1][1]}/>
+      <Piece positionXZ={[1.2, -3.1]} player={1} piece={playerStacks[1][2]}/>
     </group>
   )
 }
